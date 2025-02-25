8d4b61fe96051706496aba52456c443888ac9548
Kim Mortensen <61045621+krixzy@users.noreply.github.com>
2025-02-25 13:45:46 +0100

    Fix it plz Daniel

--- project/train_model.py
+++ project/train_model.py
@@ -1,23 +1,56 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
+
+
+
+
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline, AutoConfig, AutoModel, TrainingArguments, DataCollatorWithPadding, Trainer
+from datasets import load_dataset
 import torch
 
-model_dir = "project/DeepSeek-R1-Distill-Qwen-1.5B" 
+
+save_dir = "./finetuned_model"
+model_dir = "DeepSeek-R1-Distill-Qwen-1.5B" 
+
+def tokenize_dataset(dataset):
+    tokenized = tokenizer(dataset["text"], padding="max_length", truncation=True)
+    return tokenized
+
 
 tokenizer = AutoTokenizer.from_pretrained(model_dir)
+tokenizer.pad_token = tokenizer.eos_token
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model = AutoModelForCausalLM.from_pretrained(
     model_dir,
-    torch_dtype=torch.float16,
-).to(device)
-is_running = True
-while is_running:
-    user_input = input("Enter your prompt: ")
-    print(f"You Asked: {user_input}")
-    if user_input == "exit":
-        is_running = False
-        break
-    input_ids = tokenizer(user_input, return_tensors="pt").input_ids.to(device)
-    print("Loading...")
-    output = model.generate(input_ids, max_length=200, top_p=0.95)
-    print(tokenizer.decode(output[0], skip_special_tokens=True))
+    torch_dtype="auto"
+)
+
+
+dataset = load_dataset("rotten_tomatoes")
+dataset = dataset.map(tokenize_dataset, batched=True)
+data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+# config = AutoConfig.from_pretrained(model_dir)
+# model = AutoModel.from_config(config)
+
+training_args = TrainingArguments(
+    output_dir=save_dir,
+    learning_rate=1,
+    per_device_train_batch_size=1,
+    per_device_eval_batch_size=1,
+    num_train_epochs=1,
+    eval_strategy="epoch",
+    # use_cpu=True,
+    save_strategy="epoch"
+)
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=dataset["train"],
+    eval_dataset=dataset["test"],
+    processing_class=tokenizer,
+    data_collator=data_collator,
+)
+
+
+trainer.train()
