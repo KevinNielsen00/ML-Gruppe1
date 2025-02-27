75bd83dc3da23c11a10f0330b1f262a17afa317a
Kim Mortensen <61045621+krixzy@users.noreply.github.com>
2025-02-27 10:10:21 +0100

    Her Daniel

--- project/write_model.py
+++ project/write_model.py
@@ -4,10 +4,11 @@
 
 from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline, AutoConfig, AutoModel, TrainingArguments, DataCollatorWithPadding, Trainer
 from datasets import load_dataset
+from peft import PeftModel
 import torch
 
-# model_dir = "distilgpt2"
-model_dir = "finetuned_model/model" 
+model_dir = "models/DeepSeek-R1-Distill-Qwen-1.5B"
+finetuned_model_dir = "finetuned_lora_model" 
 
 
 tokenizer = AutoTokenizer.from_pretrained(model_dir)
@@ -19,8 +20,28 @@ model = AutoModelForCausalLM.from_pretrained(
     
 )
 
-text_generator = pipeline("text-generation", model=model, tokenizer=tokenizer)
-resault = text_generator("what is go lang", max_length=25, temperature=0.1, truncation=True)
-print(resault)
-# config = AutoConfig.from_pretrained(model_dir)
-# model = AutoModel.from_config(config)
+model = PeftModel.from_pretrained(model, finetuned_model_dir)
+
+
+model = model.merge_and_unload()
+
+def quistion():
+    print("What is your question?")
+    question = input()
+    return question
+
+def response(quistion):
+    text_generator = pipeline("text-generation", model=model, tokenizer=tokenizer)
+    resault = text_generator(quistion, max_length=512, temperature=0.5, truncation=True)
+    return resault
+
+while True:
+    a_question = quistion()
+    if a_question.lower == "exit":
+        break
+    print(f"Your question is: {a_question}")
+    a_response = response(a_question)
+    print(f"Answer: {a_response[0]['generated_text']}")
+
+    
+
